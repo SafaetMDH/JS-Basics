ee5f8a4b2d8256bf0394a9c780b5a10fd997ebe7
Safaet <SafaetMDH@gmail.com>
2023-09-25 09:37:44 -0400

    Assignment

--- funcProg.js
+++ funcProg.js
@@ -16,19 +16,24 @@ console.log(currencyTwo);
 
 //First Class Function
 
+//Function 1 'addTwoNums'
 function addTwoNums(a, b) {
     console.log(a + b)
 }
 
+// Function 2 'randomNum'
 function randomNum() {
     return Math.floor((Math.random() * 10) + 1);
 }
+
+//Function 3 'specificNum'
 function specificNum() { return 42 };
 
 var useRandom = true;
 
 var getNumber;
 
+//Function 4 
 if(useRandom) {
     getNumber = randomNum
 } else {
