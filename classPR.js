a2fe35254892c2921d2ea86049100d2ad73efeb6
Safaet <SafaetMDH@gmail.com>
2023-10-23 09:43:45 -0400

    TOPIC CLASS

---
+++ classPR.js
@@ -0,0 +1,13 @@
+
+class Train{
+  constructor(color, lightsOn){
+
+    this.color = color;
+    this.lightsOn = lightsOn;
+
+  }
+}
+
+var myFirstTrain = new Train("red", false);
+
+console.log(myFirstTrain);
